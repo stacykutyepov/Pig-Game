725fad7cc99061ebbc764ad865ab2b96787b0aec
KayhanB <kayhan.burak@outlook.com>
2020-05-08 19:10:29 +0300

    added dice animation
    just some excitement

--- src/app.js
+++ src/app.js
@@ -6,12 +6,18 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
-var scores, roundScore, activePlayer, gamePlaying, diceTwiceSix, winningScore;
+var scores, roundScore, activePlayer, gamePlaying, diceTwiceSix, winningScore, diceAnimating;
 init();
 winningScore = 100;
+loopTime     = 50;
+avarage		= 20;
+maxLoopTime  = 220;
+winningScore = 100;
 
-document.querySelector('.btn-roll').addEventListener('click', function () {
+document.querySelector('.btn-roll').addEventListener('click', async function () {
+	if (diceAnimating) return;
 	if (gamePlaying) {
+		await animateDice();
 		// Random number
 		var dice = Math.floor(Math.random() * 6) + 1;
 		// Display the result 
@@ -38,6 +44,7 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 });
 // HOLD BTN
 document.querySelector('.btn-hold').addEventListener('click', function () {
+	if (diceAnimating) return;
 	if (gamePlaying) {
 		// add current score to Global Score
 		scores[activePlayer] += roundScore;
@@ -59,6 +66,29 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 		}
 	}
 });
+
+async function animateDice() {
+  	diceAnimating = true;
+
+	var dice = Math.floor(Math.random() * 6) + 1;
+	var diceDOM = document.querySelector(".dice");
+	diceDOM.style.display = "block";
+	diceDOM.src = "../assets/images/dice-" + dice + ".png";
+
+	var diceTwo = Math.floor(Math.random() * 6) + 1;
+	var diceTwoDOM = document.querySelector(".dice-0");
+	diceTwoDOM.style.display = "block";
+	diceTwoDOM.src = "../assets/images/dice-" + diceTwo + ".png";
+
+	await new Promise((resolve) => setTimeout(resolve, loopTime));
+
+	loopTime += avarage;
+	if (loopTime < maxLoopTime) {
+		return await animateDice();
+	}
+	loopTime = 0;
+	diceAnimating = false;
+}
 
 function nextPlayer() {
 	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
